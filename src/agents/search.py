6a562042c95b9dfb3c4e2679abc24961c801f9af
Debasish Tudu <65746343+devsTudu@users.noreply.github.com>
2025-06-07 12:44:25 +0000

    corrections in message handling

--- src/agents/search.py
+++ src/agents/search.py
@@ -3,10 +3,12 @@ from langchain_core.documents import Document
 
 
 def convert_to_string(doc:Document):
-  resp = f"Reference {doc.metadata['Reference']}"
   url = "/".join(doc.metadata['Reference'].split(":"))
-  resp += f" [link](https://vedabase.io/en/library/bg/{url}) \n"
-  resp += f" {doc.metadata['Translation']} \n {doc.page_content.replace('"','')}"
+  content = doc.page_content.replace('"','')
+  resp = content
+  resp +=  f"\n\n"
+  resp += f"{doc.metadata['Reference']} _{doc.metadata['Translation']}_\n"
+  resp += f" [Chapter {doc.metadata['Chapter']} Verse {doc.metadata['Verse']}](https://vedabase.io/en/library/bg/{url}) \n"
   return resp
 
 def search_for(query:str,n=4):
@@ -14,4 +16,7 @@ def search_for(query:str,n=4):
     result =Robust.invoke(query,fetch_k = n)
     if not result:
         return "No results found"
-    return "\n\n".join([convert_to_string(doc) for doc in result])
+    resp = "Here is what I found for your query:\n"
+    for i, doc in enumerate(result):
+        resp += f"\n\n({i+1})  {convert_to_string(doc)}"
+    return resp
